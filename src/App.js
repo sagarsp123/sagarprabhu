e882556d2d35c6faa25433a158ecd2c28ba1d1a8
Sagar Prabhu <48390647+sagarsp123@users.noreply.github.com>
2023-08-15 21:34:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -344,7 +344,7 @@ const Exp5Page = () => {
 
 function App() {
   return (
-    <Router>      
+    <Router basename="/SagarPrabhu">      
         <Container>
           <Routes>
             <Route path="/" element={<Layout />} />
